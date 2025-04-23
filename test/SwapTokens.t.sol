8b626541fc4bbe051af58b04b554ef6d18ace42c
Springbock99 <daneelkent@gmail.com>
2025-04-23 11:03:38 +0200

    updated SwapToken tests to make sure the deal works correctly

--- test/SwapTokens.t.sol
+++ test/SwapTokens.t.sol
@@ -2,19 +2,24 @@
 pragma solidity ^0.8.25;
 
 import {Test, Vm, console} from "forge-std/Test.sol";
-import {IERC20} from "@openzeppelin-contracts-5.2.0/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin-contracts-5.3.0/token/ERC20/IERC20.sol";
 
 contract TokenSwapTest is Test {
     address public owner;
     address public user1;
     address public user2;
-    address public Dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public Weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    IERC20 public daiToken;
+    IERC20 public wEthTokens;
 
     function setUp() public {
         owner = makeAddr("owner");
         user1 = makeAddr("user1");
         user2 = makeAddr("user2");
+
+        daiToken = IERC20(dai);
+        wEthTokens = IERC20(weth);
 
         string memory rpcUrl = vm.envString("ETHEREUM_URL");
 
@@ -22,13 +27,19 @@ contract TokenSwapTest is Test {
 
         vm.startPrank(owner);
 
-        deal(address(Dai), user1, );
-        deal(address(Weth), user2, );
+        deal(address(dai), user1, 100 * 1e18);
+        deal(address(weth), user1, 100 * 1e18);
+        deal(address(dai), user2, 100 * 1e18);
+        deal(address(weth), user2, 100 * 1e18);
 
         vm.stopPrank();
     }
 
-    // function test_userBalance() public {
-    //     user1 balance = balance(user1);
-    // }
+    function test_userBalance() public {
+        uint256 user1BalanceDai = daiToken.balanceOf(user1);
+        uint256 user2BalanceWeth = wEthTokens.balanceOf(user2);
+
+        assertEq(user1BalanceDai, 100 * 10 ** 18);
+        assertEq(user2BalanceWeth, 100 * 10 ** 18);
+    }
 }
